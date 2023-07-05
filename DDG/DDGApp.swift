5f4676f02409e518490549bc3b116f9d7aa5cb7e
assem16399 <aasem.hany@gmail.com>
2023-07-05 09:45:54 +0300

    Initial Commit

---
+++ DDG/DDGApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDGApp.swift
+//  DDG
+//
+//  Created by Aasem Hany on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DDGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
